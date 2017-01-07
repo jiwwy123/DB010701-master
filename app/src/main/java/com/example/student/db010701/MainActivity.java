7fb083279b2250f50bc3e4d8c9d831ecb9595c3a
jiwwy <jiwwy123@gmail.com>
2017-01-07 12:13:07 +0800

    Initial commit

---
+++ app/src/main/java/com/example/student/db010701/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.student.db010701;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textView2);
+    }
+
+    public void click1(View v)
+    {
+        tv.setText("Hello World");
+    }
+}
